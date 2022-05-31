992ca9e79afd2b6c99f98ad11974c096eae4f34e
zhuliang <zhuliang@huohua.cn>
2022-05-31 21:38:24 +0800

    网关新增鉴权拦截

--- zfenrir-gateway/src/main/java/com/zfenrir/gateway/filter/gateway/GpDefineGatewayFilterFactory.java
+++ zfenrir-gateway/src/main/java/com/zfenrir/gateway/filter/gateway/GpDefineGatewayFilterFactory.java
@@ -1,12 +1,20 @@
 package com.zfenrir.gateway.filter.gateway;
 
+import com.alibaba.fastjson.JSONObject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
+
+import java.nio.charset.StandardCharsets;
 
 import reactor.core.publisher.Mono;
 /**
@@ -27,6 +35,18 @@ public class GpDefineGatewayFilterFactory extends AbstractGatewayFilterFactory<G
     public GatewayFilter apply(GpConfig config) {
         return ((exchange, chain) -> {
             pre(exchange, chain);
+            if (CollectionUtils.isEmpty(exchange.getRequest().getQueryParams().get("auth"))) {
+                ServerHttpResponse response = exchange.getResponse();
+                JSONObject message = new JSONObject();
+                message.put("status", -1);
+                message.put("data", "鉴权失败");
+                byte[] bits = message.toJSONString().getBytes(StandardCharsets.UTF_8);
+                DataBuffer buffer = response.bufferFactory().wrap(bits);
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                //指定编码，否则在浏览器中会中文乱码
+                response.getHeaders().add("Content-Type", "text/plain;charset=UTF-8");
+                return response.writeWith(Mono.just(buffer));
+            }
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 post(exchange, chain);
             }));
